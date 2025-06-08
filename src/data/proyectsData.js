be0f80b075db0a6b1abfdae31c68dad2931aee1a
Germancor <gcortez116@gmail.com>
2025-06-08 14:16:43 -0300

    data modificada

--- src/data/proyectsData.js
+++ src/data/proyectsData.js
@@ -2,21 +2,21 @@ const projectData = [
   {
     title: "Rick and Morty App",
     description: "Public API consumption with React and Styled Components.",
-    image: "../src/assets/api.jpg", 
+    image: "/icons/api.jpg", 
     link: "https://github.com/Germancor/ApiRickandMorty.git",
     live: "https://rickandmortyapi2025.netlify.app"
   },
   {
     title: "Vet",
     description: "Consultation and inspection of domestic animals",
-    image: "../src/assets/vet.jpg",
+    image: "/icons/vet.jpg",
     link: "https://github.com/Germancor/Vet2024.git",
     live: ""
   },
   {
     title: "Calculator",
     description: "React Calculator",
-    image: "../src//assets/calculator.jpg",
+    image: "/icons/calculator.jpg",
     link: "https://github.com/Germancor/CalculatorReact.git",
     live: "https://heroic-cassata-0e5c75.netlify.app/"
   }
